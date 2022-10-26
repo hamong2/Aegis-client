cdec4846b46b13fca42a2688de478d65d3545d83
Park HyeongIn <hyeongin2024@gmail.com>
2022-10-26 21:07:55 +0900

    Feat: Log Component 추가

---
+++ src/components/IterLog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+export default function IterLog(props) {
+    const rendering = () => {
+        const result = [];
+        for(let i = 0; i < props.log.length; i++) {
+            var verb, time = props.log[i];
+        }
+    }
+    return (<div></div>);
+}
